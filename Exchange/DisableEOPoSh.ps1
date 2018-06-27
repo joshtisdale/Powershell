a97ef783d24112b5043098f957cafb9c1a6098dd
Tisdale912 <joshtisdale@gmail.com>
2018-06-27 11:44:29 -0400

    upload

--- Exchange/DisableEOPoSh.ps1
+++ Exchange/DisableEOPoSh.ps1
@@ -1,3 +1,3 @@
 Get-User -ResultSize unlimited -Filter {(MemberOfGroup -eq 'TechOps')}
 
-#-and (Department -eq 'Accounting')} | Set-User -RemotePowerShellEnabled $false
+#-and (Department -eq 'Accounting')} | Set-User -RemotePowerShellEnabled $false (delete this)
